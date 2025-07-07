265764854859ead9eca54d036c3515b4fdaf0cbb
Gavin Szczesniak <gavinszczesniak@gmail.com>
2025-07-07 11:54:05 -0400

    Completes the CRUD calls for logging the exercise   - Uses Spring's mapping handlers   - Catches exception errors for arguments and value constraints   - Validates the record for our in-memory value

--- src/main/java/dev/gszczes/runnerz_demo/run/Run.java
+++ src/main/java/dev/gszczes/runnerz_demo/run/Run.java
@@ -2,11 +2,26 @@ package dev.gszczes.runnerz_demo.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
   Integer id,
+  @NotEmpty
   String title,
   LocalDateTime startedOn,
   LocalDateTime completedOn,
+  @Positive
   Integer miles,
   Location location
-) {}
+) {
+  public Run {
+    if (startedOn.isAfter(completedOn)) {
+      throw new IllegalArgumentException("Completed On must be after Started On.");
+    }
+
+    if (title.isEmpty()) {
+      throw new IllegalArgumentException("Title is empty");
+    }
+  }
+}
--- src/main/java/dev/gszczes/runnerz_demo/run/RunController.java
+++ src/main/java/dev/gszczes/runnerz_demo/run/RunController.java
@@ -1,14 +1,34 @@
 package dev.gszczes.runnerz_demo.run;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
   private final RunRepository runRepository;
@@ -17,9 +37,42 @@ public class RunController {
     this.runRepository = runRepository;
   }
 
-  @GetMapping("/api/runs")
+  @GetMapping("")
   List<Run> findAll() {
     return runRepository.findAll();
   }
+
+  @GetMapping("/{id}")
+  Run findById(@PathVariable Integer id) {
+    Optional<Run> run = runRepository.findById(id);
+
+    if (run.isEmpty()) {
+      throw new RunNotFoundException();
+    } else {
+      return run.get();
+    }
+  }
+
+  // post
+  @PostMapping("")
+  @ResponseStatus(HttpStatus.CREATED)
+  void create(@Valid @RequestBody Run run) {
+      runRepository.create(run);
+  }
+
+  // put
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  void update(@PathVariable Integer id, @Valid @RequestBody Run run) {
+      runRepository.update(run, id);
+  }
+
+  // delete
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  void delete(@PathVariable Integer id) {
+    runRepository.delete(id);
+  }
+  
   
 }
